fd22406d0af2a8733f0d55ee35012d44af599ccd
McHaris <harismanaf96@gmail.com>
2019-07-10 09:56:00 +0700

    first commit

---
+++ src/main/java/com/eksad/springbasicaut/SpringbasicautApplication.java
@@ -0,0 +1,13 @@
+package com.eksad.springbasicaut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbasicautApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbasicautApplication.class, args);
+	}
+
+}
